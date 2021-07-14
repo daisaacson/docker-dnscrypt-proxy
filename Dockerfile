1e9ba4d1c14feddc57d29318174f0de1712e7895
daisaacson <49328302+daisaacson@users.noreply.github.com>
2021-07-14 01:52:34 +0000

    initial build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:3.14.0
-RUN apk --no-cache update && apk --no-cache upgrade && apk add --no-cache bash dnscypt-proxy execline
+RUN apk --no-cache update && apk --no-cache upgrade && apk add --no-cache bash dnscrypt-proxy execline
 COPY docker-entrypoint.sh /usr/local/bin
 EXPOSE 53/tcp
 EXPOSE 53/udp
 ENTRYPOINT ["docker-entrypoint.sh"]
-CMD []
+CMD ["dnscrypt-proxy", "-config", "/etc/dnscrypt-proxy/dnscrypt-proxy.toml"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 set -eo pipefail
 
-cat > /etc/dnscrypt-proxy/dnscrypt-proxy.toml <<EOF
+# If forwarders were present, create forwarders directive
+function generate_forwarders () {
+	if [[ "$1" ]]; then
+		IFS='|' read -ra forwarders <<< "$1"
+		for forwarder in "${forwarders[@]}"; do
+			IFS=',' read domain server <<< "$forwarder"
+			FWDS+="${domain}\t${server}\n"
+		done
+		echo -en "$FWDS"
+	fi
+}
+
+_SERVERS_=${SERVERS:=quad9-doh-ip4-filter-pri}
+_FORWARDERS_=$(generate_forwarders $FORWARDERS)
+
+sed -i.dockersave \
+  -e 's/^#\s\(server_names\).*/\1 = ['"'$_SERVERS_'"']/gi;' \
+  -e 's/127.0.0.1:53/0.0.0.0:53/gi;' \
+  -e 's/^#\s\(forwarding_rules.*\)/\1/gi;' \
+  /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+cat > /etc/dnscrypt-proxy/forwarding-rules.txt <<EOF
+$_FORWARDERS_
 EOF
 
 exec "$@"
